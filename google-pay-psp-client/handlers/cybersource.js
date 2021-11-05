99196d4f621d8152e5ffa945fa17e9aba74c44c1
dackers86 <ackers86@hotmail.com>
2021-11-05 21:01:11 +0000

    fix: added cybersource patch logger

--- google-pay-psp-client/handlers/cybersource.js
+++ google-pay-psp-client/handlers/cybersource.js
@@ -14,30 +14,30 @@
  * limitations under the License.
  */
 
-const cybersource = require('cybersource-rest-client');
-
-// Patch logging, per:
-// https://github.com/CyberSource/cybersource-rest-client-node/issues/66
-const winston = require('winston');
-cybersource.Logger.getLogger = () => winston.createLogger({
-  transports: [new winston.transports.Console({silent: true})]
-});
+const cybersource = require("cybersource-rest-client");
+const { getLogger } = require("../patch-logger");
 
 module.exports = (config, order) => {
   // See PSP's docs for full API details:
   // https://docs.cybersource.com/en/payments-tech-docs/googlepay.html
 
-  const client = new cybersource.PaymentsApi({
-    authenticationType: config.authenticationType,
-    runEnvironment: config.runEnvironment,
-    merchantID: config.merchantID,
-    merchantKeyId: config.merchantKeyId,
-    merchantsecretKey: config.merchantsecretKey,
-  }, new cybersource.ApiClient());
+  cybersource.Logger.getLogger = getLogger;
+
+  const client = new cybersource.PaymentsApi(
+    {
+      authenticationType: config.authenticationType,
+      runEnvironment: config.runEnvironment,
+      merchantID: config.merchantID,
+      merchantKeyId: config.merchantKeyId,
+      merchantsecretKey: config.merchantsecretKey,
+      enableLog: false,
+    },
+    new cybersource.ApiClient()
+  );
 
   const paymentRequest = {
     processingInformation: {
-      paymentSolution: '012', // signifies Google Pay being used
+      paymentSolution: "012", // signifies Google Pay being used
     },
     orderInformation: {
       amountDetails: {
@@ -47,13 +47,15 @@ module.exports = (config, order) => {
     },
     paymentInformation: {
       fluidData: {
-        value: Buffer.from(JSON.stringify(order.paymentToken)).toString('base64'),
+        value: Buffer.from(JSON.stringify(order.paymentToken)).toString(
+          "base64"
+        ),
       },
     },
   };
 
   return new Promise((resolve, reject) => {
-    client.createPayment(paymentRequest, function(error, data, response) {
+    client.createPayment(paymentRequest, function (error, data, response) {
       if (!error) {
         resolve(data);
       } else {
---
+++ google-pay-psp-client/patch-logger.js
@@ -0,0 +1,55 @@
+const winston = require("winston");
+const { format } = require("winston");
+const { combine, timestamp, label, printf } = format;
+require("winston-daily-rotate-file");
+
+const loggingFormat = printf(({ level, message, label, timestamp }) => {
+  return `[${timestamp}] [${level.toUpperCase()}] [${label}] : ${message}`;
+});
+
+function getLogger(merchantConfig, loggerCategory = "UnknownCategoryLogger") {
+  var appTransports = createTransportFromConfig(merchantConfig);
+
+  var loggingLevel = merchantConfig.getLoggingLevel();
+  var enableLog = merchantConfig.getEnableLog();
+  return winston.loggers.get(loggerCategory, {
+    level: loggingLevel,
+    silent: !enableLog,
+    format: combine(
+      label({ label: loggerCategory }),
+      timestamp(),
+      loggingFormat
+    ),
+    transports: appTransports,
+  });
+}
+
+function createTransportFromConfig(mConfig) {
+  var transports = [];
+
+  var loggingLevel = mConfig.getLoggingLevel();
+  var maxLogFiles = mConfig.getMaxLogFiles();
+  var logFileName = mConfig.getLogFileName();
+  var logDirectory = mConfig.getLogDirectory();
+  var enableLog = mConfig.getEnableLog();
+
+  transports.push(
+    enableLog
+      ? new winston.transports.DailyRotateFile({
+          level: loggingLevel,
+          filename: logFileName + "-%DATE%.log",
+          datePattern: "YYYY-MM-DD",
+          zippedArchive: true,
+          dirname: logDirectory,
+          maxFiles: maxLogFiles,
+          silent: !enableLog,
+        })
+      : new winston.transports.Console({
+          format: winston.format.simple({}),
+        })
+  );
+
+  return transports;
+}
+
+module.exports.getLogger = getLogger;
