a7e9f29946ff59590d534d8896652d3c6d4977fe
Stephen McDonald <steve@jupo.org>
2021-12-01 14:01:27 +1100

    Formatting

--- google-pay-psp-client/handlers/datatrans.js
+++ google-pay-psp-client/handlers/datatrans.js
@@ -3,25 +3,28 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 module.exports = (config, order) => {
 
-    const requestData = {
-        currency: order.currency,
-        amount: order.totalInt,
-        PAY: order.paymentToken,
-        refno: shortid.generate()
-    };
+  const requestData = {
+    currency: order.currency,
+    amount: order.totalInt,
+    PAY: order.paymentToken,
+    refno: shortid.generate()
+  };
 
-    return fetch(`https://api${config.environment === 'sandbox' ? '.sandbox' : ''}.datatrans.com/v1/transactions/authorize`, {
-        method: "POST",
-        body: JSON.stringify(requestData),
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Basic ${Buffer.from(`${config.clientConfig.gatewayMerchantId}:${config.apiKey}`).toString('base64')}`
-        }
-    }).then(response => {
-        if (response.ok) {
-            return Promise.resolve(response.json());
-        } else {
-            return Promise.reject(response.json());
-        }
-    });
+  const host = config.environment === 'sandbox' ? 'api.sandbox' : 'api';
+  const auth = Buffer.from(`${config.clientConfig.gatewayMerchantId}:${config.apiKey}`).toString('base64');
+
+  return fetch(`https://${host}.datatrans.com/v1/transactions/authorize`, {
+    method: "POST",
+    body: JSON.stringify(requestData),
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Basic ${auth}`
+    }
+  }).then(response => {
+    if (response.ok) {
+      return Promise.resolve(response.json());
+    } else {
+      return Promise.reject(response.json());
+    }
+  });
 };
